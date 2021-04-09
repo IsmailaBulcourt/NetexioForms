c1579fd89d96f5da23991e0dcd6df1fd2319c35f
darksasukedu59 <44493087+darksasukedu59@users.noreply.github.com>
2021-04-09 22:28:58 +0100

    Ajoutez des fichiers projet.

---
+++ NetexioFormulaire/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetexioFormulaire.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

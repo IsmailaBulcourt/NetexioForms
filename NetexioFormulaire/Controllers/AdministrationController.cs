83c77bd4c556b23357bf74bf7db9ac0dec80eda5
darksasukedu59 <44493087+darksasukedu59@users.noreply.github.com>
2021-04-09 22:53:51 +0100

    retour a la normale

---
+++ NetexioFormulaire/Controllers/AdministrationController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetexioFormulaire.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateFormation()
+        {
+            return View();
+        }
+
+        public IActionResult CreateSession()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteFormation()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteSession()
+        {
+            return View();
+        }
+
+        public IActionResult SetActiveSession()
+        {
+            return View();
+        }
+    }
+}
---
+++ NetexioFormulaire/Controllers/FormsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetexioFormulaire.Controllers
+{
+    public class FormsController : Controller
+    {
+        public ActionResult FormsPage1()
+        {
+            return View();
+        }
+
+        public ActionResult FormsPage2()
+        {
+            return View();
+        }
+    }
+}
